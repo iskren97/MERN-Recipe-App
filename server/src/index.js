877a769df889830b61494174b9ea22e17b9c0d50
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-03-10 16:43:23 +0200

    add .env variables, connect to db

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(
+  `mongodb+srv://iskren97:${process.env.DB_PASS}@recipes-app.ofwehet.mongodb.net/recipes-app?retryWrites=true&w=majority`
+);
 
 app.listen(3002, () => console.log('Listening on port 3002'));
