d8eddb7aca57a744f874bf3c55bff8cab3c5848f
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-03-10 19:16:03 +0200

    complete the initial routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Auth } from './pages/Auth';
+import CreateRecipe from './pages/CreateRecipe';
 import Home from './pages/Home';
+import SavedRecipes from './pages/SavedRecipes';
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </BrowserRouter>
     </div>
