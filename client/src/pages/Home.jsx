485a5a22fc7587b3b37245d2d8a8130c7d9a94ca
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-03-14 16:23:34 +0200

    fetch and display recipes on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get('http://localhost:3002/recipes/');
+        setRecipes(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchRecipes();
+  }, []);
+
+  return (
+    <>
+      <h1>Recipes</h1>
+
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h3>{recipe.name}</h3>
+              <button>Save</button>
+            </div>
+
+            <div>
+              <p>{recipe?.instructions}</p>
+            </div>
+
+            <img
+              src={recipe.imgUrl}
+              alt=""
+              style={{ height: '220px', width: '200px', objectFit: 'cover' }}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Home;
