1b292dade1135ec91dbdf31e3dbc6b46c0549956
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-03-14 13:20:53 +0200

    add recipe router

--- server/src/index.js
+++ server/src/index.js
@@ -5,12 +5,14 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { userRouter } from './routes/users.js';
+import { recipeRouter } from './routes/recipes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use('/users', userRouter);
+app.use('/recipes', recipeRouter);
 
 mongoose
   .connect(
