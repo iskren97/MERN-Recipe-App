41446b5c79309fcc8e3d7e74c8b211a3f97b3d9d
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-03-14 13:52:41 +0200

    add create and read recipes routes

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,5 +1,31 @@
 import express from 'express';
+import RecipeModel from '../models/Recipes.js';
 
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const recipes = await RecipeModel.find({});
+
+    if (recipes.length === 0) {
+      throw new Error('No recipes yet');
+    }
+
+    res.send(recipes);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/create', async (req, res) => {
+  try {
+    const recipe = new RecipeModel(req.body);
+
+    await recipe.save();
+    res.send(recipe);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 export { router as recipeRouter };
