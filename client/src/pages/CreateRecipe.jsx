0934864377a66d3704043e843ec060d8cff974fd
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-03-14 15:48:06 +0200

    create recipe component and functionality

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useGetUserID } from '../hooks/useGetUserID';
+import { useNavigate } from 'react-router-dom';
 
 const CreateRecipe = () => {
-  return <div>CreateRecipe</div>;
+  const userID = useGetUserID();
+  const navigate = useNavigate();
+
+  const [recipe, setRecipe] = useState({
+    name: '',
+    ingredients: [],
+    imageUrl: 'alt',
+    userOwner: userID,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
+  const handleIngredientChange = (e, index) => {
+    const { value } = e.target;
+    const ingredients = recipe.ingredients;
+    ingredients[index] = value;
+    setRecipe({ ...recipe, ingredients });
+  };
+
+  const addIngredient = () => {
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ''] });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(recipe);
+
+    try {
+      await axios.post('http://localhost:3002/recipes/create', recipe);
+
+      alert('Recipe created!');
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h2>Create Recipe</h2>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" onChange={handleChange} />
+
+        <label htmlFor="ingredients">Ingredients</label>
+        {recipe.ingredients.map((ingredient, index) => (
+          <input
+            key={index}
+            type="text"
+            name="ingredients"
+            value={ingredient}
+            onChange={(e) => handleIngredientChange(e, index)}
+          />
+        ))}
+        <button onClick={addIngredient} type="button">
+          Add Ingredient
+        </button>
+
+        <button type="Submit">Create Recipe</button>
+      </form>
+    </>
+  );
 };
 
 export default CreateRecipe;
